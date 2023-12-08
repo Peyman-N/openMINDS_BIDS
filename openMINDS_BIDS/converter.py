7ec965b32bcf4a6b421006d4b7ffa27e9f0d17c3
Peyman-N <najafe.peyman@gmail.Com>
2023-12-08 12:57:28 +0100

    initial commit

--- openMINDS_BIDS/converter.py
+++ openMINDS_BIDS/converter.py
@@ -8,6 +8,87 @@ import pandas as pd
 import numpy as np
 import datetime
 
+def read_json(file_path: str) -> dict:
+  """
+  Reads the content of a JSON file and returns it as a Python dictionary.
+
+  Parameters:
+  - file_path (str): The path to the JSON file.
+
+  Returns:
+  - dict: A Python dictionary containing the content of the JSON file.
+
+  Example:
+  >>> data = read_json('example.json')
+  >>> print(data)
+  {"Name": "The mother of all experiments" , "BIDSVersion": "1.6.0", "DatasetType": "raw" , "License": "CC0" , "Authors": ["Paul Broca" , "Carl Wernicke" ]}
+  """
+  try:
+      # Open the JSON file
+      with open(file_path, 'r') as file:
+          # Load the JSON content into a dictionary
+          json_dic = json.load(file)
+      return json_dic
+  except FileNotFoundError:
+      # Handle file not found error
+      print(f"Error: File not found at {file_path}")
+      return {}
+  except json.JSONDecodeError:
+      # Handle JSON decoding error
+      print(f"Error: Unable to decode JSON content from {file_path}")
+      return {}
+
+def table_filter (dataframe:pd.DataFrame,filter_str:str,column:str="suffix"):
+  """
+  Filters a Pandas DataFrame based on a specified condition.
+
+  Parameters:
+  - dataframe (pd.DataFrame): The DataFrame to be filtered.
+  - filter_str (str): The value to filter the DataFrame on.
+  - column (str, optional): The column name to apply the filter on. Default is "suffix".
+
+  Returns:
+  - pd.DataFrame: A filtered DataFrame containing only the rows that satisfy the condition.
+  """
+  try:
+      # Apply the filter condition on the specified column
+      filtered_dataframe = dataframe[dataframe[column] == filter_str]
+      return filtered_dataframe
+  except KeyError:
+      # Handle the case where the specified column is not present in the DataFrame
+      KeyError(f"Error: Column '{column}' not found in the DataFrame.")
+
+def unique_items (dataFrame,colume):
+    return dataFrame[colume].unique()
+
+
+def experimental_approach_openminds (BIDS_layout_dataFrame):
+
+  experimental_approach_dic={"func":"neuroimaging",
+  "dwi":["neuroimaging","neural connectivity"],
+  "fmap":"neuroimaging",
+  "anat":["neuroimaging","anatomy"],
+  "perf":"neuroimaging",
+  "meg":"neuroimaging",
+  "eeg":"electrophysiology",
+  "ieeg":"electrophysiology",
+  "beh":"behavior",
+  "pet":"radiology",
+  "micr":"microscopy",
+  "nirs":"neuroimaging"}
+
+  approach_openminds=[]
+  approaches=unique_items(BIDS_layout_dataFrame,"datatype")
+  
+
+  for approach in approaches:
+     
+
+
+
+
+
+
 def tequniques_openmind (teqnique_BIDS):
 
 
@@ -24,14 +105,14 @@ def subject_creation ():
 
 def subjects_creation (subjects_id,layout_df):
 
-
-  participants_paths=layout_df[layout_df['suffix']=='participants']
-
-  participants_path_tsv=participants_paths[participants_paths['extension']==".tsv"].iat[0,0]
+  #Find the participants files in the files table
+  participants_paths=table_filter(layout_df,'participants')
+  #Select the tsv file of the table
+  participants_path_tsv=table_filter(participants_paths,'.tsv','extension').iat[0,0]
 
   participants_table=pd.read_csv(participants_path_tsv, sep='\t', header=0)
 
-  for 
+  for
 
 
 
@@ -64,15 +145,31 @@ def person_create(persons_list):
       )
     return new_person
   else:
-    warn("The Authour section of the BIDS- was ") 
+    warn("The Authour section of the BIDS-dataset description file was emphty") 
 
 
 def dataset_version_create (bids_layout,dataset_description,layout_df):
   
   dataset_version=core.DatasetVersion()
 
+  #Fetch the dataset type from dataset description file 
   if "DatasetType" in dataset_description:
-    dataset_type=SemanticDataType.by_name(dataset_description["DatasetType"])
+    dataset_type=controlledTerms.SemanticDataType.by_name(dataset_description["DatasetType"])
+  
+  #Fetch the digitalIdentifier from dataset description file 
+  if "DatasetDOI" in dataset_description:
+    digital_identifier=core.DOI(
+      identifier=dataset_description["DatasetDOI"])
+
+  if "EthicsApprovals" in dataset_description:
+    #to be compleated ethics_assessment
+    ethics_assessment=controlledTerms.EthicsAssessment.by_name("EU compliant")
+
+  
+
+  #Detect th
+  experimental_approach
+
 
 
 
@@ -112,10 +209,9 @@ def convert(bids_dir, openminds_dir):
   tasks=bids_layout.get_task()
 
   #imprting the datset description file containing some of the 
-  dataset_description_path=layout_df[layout_df['suffix']=='description']
-  dataset_description_f=open(dataset_description_path.iat[0,0])
-  dataset_description=json.load(dataset_description_f)
-  dataset_description_f.close()
+  dataset_description_path=table_filter(layout_df,'description')
+
+  dataset_description=read_json(dataset_description_path.iat[0,0])
 
   dataset_version=dataset_version_create(bids_layout,dataset_description,layout_df)
 

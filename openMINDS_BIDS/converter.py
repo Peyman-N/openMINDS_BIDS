70ac5a8a09a519e04c23a177676fa7b0d0aad7a9
Peyman-N <najafe.peyman@gmail.Com>
2023-12-08 02:06:08 +0100

    initial commit

--- openMINDS_BIDS/converter.py
+++ openMINDS_BIDS/converter.py
@@ -1,30 +1,44 @@
 from bids import BIDSLayout,BIDSValidator
-from openminds import core
+from openminds import core,controlledTerms
 from warnings import warn
 
+import json
 import os
 import pandas as pd
 import numpy as np
 import datetime
 
-def file
+def tequniques_openmind (teqnique_BIDS):
+
+
+def file_creation ():
+
+
+def subject_state_creation ():
+  
+
+def subject_creation ():
 
 
 
 
+def subjects_creation (subjects_id,layout_df):
 
-def subject_creation (subjects_id,layout_dump):
 
-  participants_paths=layout_dump[layout_dump['suffix']=='participants']
-  participants_path_tsv=participants_paths[participants_paths['extension']==".json"].iat[0,0]
+  participants_paths=layout_df[layout_df['suffix']=='participants']
+
+  participants_path_tsv=participants_paths[participants_paths['extension']==".tsv"].iat[0,0]
 
   participants_table=pd.read_csv(participants_path_tsv, sep='\t', header=0)
 
+  for 
 
 
 
 
-  return subject
+
+
+  return subjects
 
 def person_create(persons_list):
 
@@ -53,15 +67,33 @@ def person_create(persons_list):
     warn("The Authour section of the BIDS- was ") 
 
 
-def dataset_version_create (BIDSLayout):
+def dataset_version_create (bids_layout,dataset_description,layout_df):
+  
   dataset_version=core.DatasetVersion()
+
+  if "DatasetType" in dataset_description:
+    dataset_type=SemanticDataType.by_name(dataset_description["DatasetType"])
+
+
+
+  #creating a list containig all the Modalities used in this dataset
+  suffixs=layout_df['suffix'].unique()
+  techniques=tequniques_openmind(suffixs)
+
+
+
     
 
   return dataset_version
 
+
+
 def dataset_create ():
   
   return dataset
+
+
+
 
 def convert(bids_dir, openminds_dir):
   
@@ -70,3 +102,26 @@ def convert(bids_dir, openminds_dir):
   
   if not(BIDSValidator().is_bids(bids_dir)):
     raise NotADirectoryError(f"The input directory is not valid, you have specified {bids_dir} which is not a BIDS directory.")
+  
+  bids_layout=BIDSLayout(bids_dir)
+
+  layout_df=bids_layout.to_df()
+
+  subjects=bids_layout.get_subjects()
+
+  tasks=bids_layout.get_task()
+
+  #imprting the datset description file containing some of the 
+  dataset_description_path=layout_df[layout_df['suffix']=='description']
+  dataset_description_f=open(dataset_description_path.iat[0,0])
+  dataset_description=json.load(dataset_description_f)
+  dataset_description_f.close()
+
+  dataset_version=dataset_version_create(bids_layout,dataset_description,layout_df)
+
+
+
+
+
+
+
